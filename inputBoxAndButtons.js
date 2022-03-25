f7f5c03180d255d74e8cbf312af8925b88cc68a9
Jacqueline Tatiani <77998671+jacqueqa@users.noreply.github.com>
2022-03-25 16:45:59 +0000

    Arrays

--- inputBoxAndButtons.js
+++ inputBoxAndButtons.js
@@ -4,20 +4,30 @@
 <button>Can you guess the number? </button>
 
 <script>
-	let number = Math.round(Math.random()*10);
+	var numbers = [4, 8, 9, 5];
+
 	var input = document.querySelector("input");
+	input.focus();
 	
 	function verify() {
+		var find = false;
 
-	if(input.value == number) {
-		alert("Right Choice :D");
+		for(var i = 0; i < numbers.length; i++) {
+			if(input.value == numbers[i]) {
+				alert("Right Choice :D");
+				find = true;
+				break;
+			}	
+		}
+
+		if (find == false) {
+			alert("You are wrong =(");
+		}
+
+		input.value = "";
+		input.focus(); 
 	}
-	else {
-		alert("You are wrong =(")
-	}
-	input.value = "";
-	input.focus(); 
-}
+	
 	var button = document.querySelector("button");
 
 	button.onclick = verify;
